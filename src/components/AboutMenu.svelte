f834419fd5c2f0e32c2ef39c3df7f3775fc5080b
Wahyu Miftahul Aflah <wahyu.miftahul@dansmultipro.com>
2023-08-05 01:18:56 +0700

    add about side bar

---
+++ src/components/AboutMenu.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+    export let items: string[];
+    export let menu: string;
+    export let folderColor: string;
+
+	let visible = false;
+
+	function toggleVissible() {
+		visible = !visible;
+	}
+</script>
+
+<button class="py-3 pl-10 text-lg w-full text-start text-secondary1" class:text-white={visible} on:click={toggleVissible}>
+    {#if visible}
+        <i class="ri-folder-open-line {folderColor}"></i>
+    {:else}
+        <i class="ri-folder-2-line {folderColor}"></i>
+    {/if}
+    {menu}
+</button>
+{#if visible}
+	<ul class="list-style-none text-lg">
+        {#each items as item}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<li class="pl-16 text-secondary1 my-1 cursor-pointer" on:click={toggleVissible}>
+            <i class="ri-file-code-line"></i>
+            {item}
+        </li>
+        {/each}
+	</ul>
+{/if}
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import AboutMenu from "../../components/AboutMenu.svelte";
+
+    let data = [
+        {
+            menu: "personal-info",
+            items: ["bio.go", "interests.go", "education.go"],
+            folderColor: "text-accent1",
+        },
+        {
+            menu: "personal-info",
+            items: ["bio.go", "interests.go", "education.go"],
+            folderColor: "text-accent2",
+        },
+        {
+            menu: "personal-info",
+            items: ["bio.go", "interests.go", "education.go"],
+            folderColor: "text-accent4",
+        }
+    ]
+</script>
+
+<div class="grid grid-cols-4">
+    <div class="col-span-1 outline-default py-8">
+        {#each data as value}
+            <AboutMenu menu={value.menu} items={value.items} folderColor={value.folderColor}/>
+        {/each}
+    </div>
+</div>
